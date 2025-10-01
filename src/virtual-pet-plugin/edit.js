ddcdbdf3b65dbb7e333f89e30dc8dfe6e07593e0
lisaheinzman <lisaheinzman@knights.ucf.edu>
2025-10-01 17:36:24 -0400

    Added front-end content and layout to save.js adapted from demo html version

--- src/virtual-pet-plugin/edit.js
+++ src/virtual-pet-plugin/edit.js
@@ -1,41 +1,13 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
+import { useBlockProps, RichText } from "@wordpress/block-editor";
+import "./editor.scss";
 
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
+export default function edit({ attributes, setAttributes }) {
+  const blockProps = useBlockProps();
 
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './editor.scss';
-
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit() {
-	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Virtual Pet Plugin – hello from the editor!',
-				'virtual-pet-plugin'
-			) }
-		</p>
-	);
+  return (
+    <div {...blockProps}>
+      <p class="pet-name">Pet Name</p>
+    </div>
+  );
 }
--- src/virtual-pet-plugin/save.js
+++ src/virtual-pet-plugin/save.js
@@ -4,21 +4,78 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, RichText } from "@wordpress/block-editor";
+import turkeyLeg from "../../assets/turkey-leg.png";
+import heart from "../../assets/heart-icon.png";
+import cat from "../../assets/cat.png";
 
-/**
- * The save function defines the way in which the different attributes should
- * be combined into the final markup, which is then serialized by the block
- * editor into `post_content`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
- *
- * @return {Element} Element to render.
- */
-export default function save() {
-	return (
-		<p { ...useBlockProps.save() }>
-			{ 'Virtual Pet Plugin – hello from the saved content!' }
-		</p>
-	);
+export default function save({ attributes }) {
+  const blockProps = useBlockProps.save();
+
+  return (
+    <div {...blockProps}>
+      {/* Output the text stored in the content attribute */}
+      <div id="page-container">
+        <div id="header-container">
+          <h1 id="header-title">MySite.com</h1>
+          <div>
+            <h1 id="close-button">X</h1>
+          </div>
+        </div>
+        <h2>Pet Name</h2>
+        <div id="center-container">
+          <img id="pet-image" src={cat} alt="cat" />
+          <div id="skills-container">
+            <p id="skill-title">Pet Stats</p>
+            <div class="progress-container">
+              <img class="progress-icon" src={heart} alt="heart icon" />
+              <div class="progress-bar" id="happy">
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+              </div>
+            </div>
+            <div class="progress-container">
+              <img class="progress-icon" src={turkeyLeg} alt="turkey leg" />
+              <div class="progress-bar" id="hunger">
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+                <div class="progress-square"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="button-container">
+          <button
+            type="button"
+            onclick="handlePetClick('happy')"
+            class="action-button"
+          >
+            Pet
+          </button>
+          <button
+            type="button"
+            onclick="handlePetClick('hunger')"
+            class="action-button"
+          >
+            Treat
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
