cf978897c7e0a66f279114a43d5b3c9b94781ee0
lisaheinzman <lisaheinzman@knights.ucf.edu>
2025-10-29 13:58:36 -0400

    Add decayProgress function Add animate function and animations Restructured handleClick function Removed changePetState function Add comments to view.js

--- src/virtual-pet-plugin/view.js
+++ src/virtual-pet-plugin/view.js
@@ -1,47 +1,72 @@
 import { useBlockProps, RichText } from "@wordpress/block-editor";
-/**
- * Use this file for JavaScript code that you want to run in the front-end
- * on posts/pages that contain this block.
- *
- * When this file is defined as the value of the `viewScript` property
- * in `block.json` it will be enqueued on the front end of the site.
- *
- * Example:
- *
- * ```js
- * {
- *   "viewScript": "file:./view.js"
- * }
- * ```
- *
- * If you're not making any changes to this file because your project doesn't need any
- * JavaScript running in the front-end, then you should delete this file and remove
- * the `viewScript` property from `block.json`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
- */
+import catPet from "../../assets/cat-sparkle.gif";
+import catEat from "../../assets/cat-eat.gif";
+import catNormal from "../../assets/cat.png";
 
-//const { progress } = attributes;
-let progress = {
+// Object that defines all possible states of the virtual pet
+const petStates = {
+  NORMAL: "normal", // default state
+  HAPPY: "happy", // when progress = 10/10
+  SAD: "hunger", // when progress = 0/10
+  SLEEPY: "sleeping", // when time since last interaction is more than 30 seconds
+  BLINK: "blinking",
+  PET: "petted", // when cat is being pet
+  EAT: "eating", // when cat is given treat
+};
+
+// Object that defines the two types of progress
+const progress = {
   hunger: 0,
   happy: 0,
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  // Find all action buttons on the page
-  const actionButtons = document.querySelectorAll(".action-button");
+const petImage = document.getElementById("pet-image");
 
-  actionButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      // Capture data-type of button clicked
-      const type = event.target.dataset.type;
-      console.log("button");
-      // function call to addProgress that passes the data-type of button clicked
-      addProgress(type);
-    });
-  });
-});
+function displayPetState(state) {
+  switch (state) {
+    case petStates.NORMAL:
+      petImage.src = catNormal;
+      break;
+    case petStates.PET:
+      petImage.src = catPet;
+      break;
+    case petStates.EAT:
+      petImage.src = catEat;
+      break;
+  }
+}
 
+/*
+ * Changes button attribute disabled to "true"
+ * Button attributes changed to "false" after timeout is finished
+ */
+function disableButtons() {
+  const buttons = document.querySelectorAll(".action-button");
+  console.log(buttons);
+  buttons.forEach((button) => (button.disabled = true));
+  setTimeout(function () {
+    buttons.forEach((button) => (button.disabled = false));
+  }, 2000);
+}
+
+/*
+ * Changes state of the pet with the cooresponding button pressed
+ * Disables all action buttons to prevent overlapping animations
+ */
+function animateEvent(barID) {
+  if (barID === "hunger") {
+    displayPetState(petStates.EAT);
+  }
+  if (barID === "happy") {
+    displayPetState(petStates.PET);
+  }
+}
+
+/*
+ * Adds 1 to cooresponding progress type
+ * Adds the 'filled' class to the cooresponding progress square
+ * Checks to see if progress container is full to change pet state to HAPPY
+ */
 function addProgress(type) {
   const barType = document.getElementById(type);
   const square = barType.querySelectorAll(".progress-square");
@@ -50,14 +75,69 @@ function addProgress(type) {
     square[progress[type]].classList.add("filled");
     progress[type]++;
   }
-  /*
-  if (progress[hunger] === square.length) {
-    changeState(petStates.HAPPY);
+  if (progress[type] === square.length) {
+    displayPetState(petStates.HAPPY);
   } else {
-    changeState(petStates.NORMAL);
+    displayPetState(petStates.NORMAL);
   }
-    */
 }
+
+/*
+ * Subtracts 1 from cooresponding progress type
+ * Removes the 'filled' class to the cooresponding progress square
+ * TO ADD: Checks to see if progress container is empty to change pet state to SAD
+ */
+function decayProgress(barId) {
+  console.log(progress);
+  const barType = document.getElementById(barId);
+  const square = barType.querySelectorAll(".progress-square");
+  if (progress[barId] > 0) {
+    progress[barId]--;
+    console.log(square[progress[barId]]);
+    square[progress[barId]].classList.remove("filled");
+  }
+}
+
+clearInterval();
+
+const intervals = {};
+// delays the decay of progress
+intervals["happy"] = setInterval(() => decayProgress("happy"), 20000);
+intervals["hunger"] = setInterval(() => decayProgress("hunger"), 20000);
+
+/*
+ * Handles a button interaction by calling functions that:
+ * - Animate the corresponding event
+ * - Add progress to the corresponding progress bar after brief pause for the animation
+ * - Reset the decay timer (delays the decay of progress)
+ * - Disable all buttons during animation
+ */
+function handlePetClick(barID) {
+  console.log(barID);
+  clearInterval(intervals[barID]);
+  animateEvent(barID);
+  setTimeout(() => addProgress(barID), 2000);
+  disableButtons();
+  intervals[barID] = setInterval(() => decayProgress(barID), 20000);
+}
+
+/*
+ * Function exlusive to WordPress version because HTML in save.js becomes static, resulting in unresponsive buttons
+ * Finds all action buttons on the page
+ * Captures data-type of button clicked in 'type'
+ * Calls handler function that
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  const actionButtons = document.querySelectorAll(".action-button");
+  actionButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const type = event.target.dataset.type;
+      console.log("button");
+      // function call to addProgress that passes the data-type of button clicked
+      handlePetClick(type);
+    });
+  });
+});
 
 /* eslint-disable no-console */
 console.log("Hello World! (from create-block-virtual-pet-plugin block)");
